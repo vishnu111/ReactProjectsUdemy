91089523a33ae02bbf115a9568b1419edd2b7c02
Vishnu Lokesh Reddy <vishnulokesh520@gmail.com>
2023-04-09 02:12:53 +0530

    react js project completion

--- media/src/store/apis/photosApi.js
+++ media/src/store/apis/photosApi.js
@@ -8,6 +8,13 @@ const photosApi = createApi({
   endpoints(builder) {
     return {
       fetchPhotos: builder.query({
+        providesTags: (result, error, album) => {
+          const tags = result.map((photo) => {
+            return { type: "Photo", id: photo.id };
+          });
+          tags.push({ type: "AlbumPhoto", id: album.id });
+          return tags;
+        },
         query: (album) => {
           return {
             url: "/photos",
@@ -20,6 +27,9 @@ const photosApi = createApi({
         },
       }),
       addPhoto: builder.mutation({
+        invalidatesTags: (result, error, album) => {
+          return [{ type: "AlbumPhoto", id: album.id }];
+        },
         query: (album) => {
           return {
             method: "POST",
@@ -32,6 +42,9 @@ const photosApi = createApi({
         },
       }),
       removePhoto: builder.mutation({
+        invalidatesTags: (result, error, photo) => {
+          return [{ type: "Photo", id: photo.id }];
+        },
         query: (photo) => {
           return {
             method: "DELETE",
